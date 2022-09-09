3f125440daaf21dbe4df23ca476de44cb0704ba2
Zafer Balkan <zafer@zaferbalkan.com>
2022-09-09 16:52:01 +0300

    Major rewrite

--- nmap_scan.py
+++ nmap_scan.py
@@ -6,44 +6,102 @@
 # Do NOT include subnets with a network firewall in the path of the agent and the subnet.
 ################################
 import json
-import time
+import logging
+import os
+import platform
+import sys
+from datetime import datetime
 
 import nmap
 
-with open("subnets.json", "r") as read_file:
-    data = json.load(read_file)
 
-scanner: nmap.PortScanner = nmap.PortScanner()
-# Add subnets to scan to the Subnets Array
-subnets: list[str] = data.get("subnets")
-for subnet in subnets:
-    json_output = {}
-    # scanner.scan(subnet)
-    for host in scanner.all_hosts():
-        json_output['nmap_host'] = host
-        for proto in scanner[host].all_protocols():
-            if proto not in ["tcp", "udp"]:
-                continue
-            json_output['nmap_protocol'] = proto
-            lport: list = list(scanner[host][proto].keys())
-            lport.sort()
-            for port in lport:
-                hostname: str = ""
-                json_output['nmap_port'] = port
-                for h in scanner[host]["hostnames"]:
-                    hostname: str = h["name"]
-                    json_output['nmap_hostname'] = hostname
-                    hostname_type: str = h["type"]
-                    json_output['nmap_hostname_type'] = hostname_type
-                    json_output['nmap_port_name'] = scanner[host][proto][port]["name"]
-                    json_output['nmap_port_state'] = scanner[host][proto][port]["state"]
-                    json_output['nmap_port_product'] = scanner[host][proto][port]["product"]
-                    json_output['nmap_port_extrainfo'] = scanner[host][proto][port]["extrainfo"]
-                    json_output['nmap_port_reason'] = scanner[host][proto][port]["reason"]
-                    json_output['nmap_port_version'] = scanner[host][proto][port]["version"]
-                    json_output['nmap_port_conf'] = scanner[host][proto][port]["conf"]
-                    json_output['nmap_port_cpe'] = scanner[host][proto][port]["cpe"]
-                    with open("/var/ossec/logs/active-responses.log", "a") as active_response_log:
-                        active_response_log.write(json.dumps(json_output))
-                        active_response_log.write("\n")
-                time.sleep(2)
+def main() -> None:
+    # Read configuration
+    subnetsPath: str = os.path.dirname(
+        os.path.realpath(__file__)) + "/subnets.json"
+    with open(subnetsPath, "r") as read_file:
+        data = json.load(read_file)
+
+    subnets: list = data.get("subnets")
+    verbose: bool = data.get("verbose")
+
+    logpath: str = detect_logpath()
+
+    root_logger: logging.Logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    handler: logging.FileHandler = logging.FileHandler(logpath, 'w', 'utf-8')
+    handler.setFormatter(logging.Formatter('%(message)s'))
+    root_logger.addHandler(handler)
+
+    excepthook = logging.error
+
+    # Initiate scan
+    scanner: nmap.PortScannerYield = nmap.PortScannerYield()
+    for subnet in subnets:
+        # Log per subnet
+        if (verbose):
+            log_debug("Starting scan over in subnet: " +
+                      subnet, verbose=verbose)
+
+        for host, result in scanner.scan(subnet):
+            if (verbose):
+                print("Reporting host: " + host)
+            log_info(result, verbose=verbose)
+
+    if (verbose):
+        log_debug("Nmap scan completed.", verbose=verbose)
+
+
+def detect_logpath() -> str:
+    logDir: str = os.path.join('/var/log', 'nmap_scan')
+    if (platform.system() == "Windows"):
+        logDir: str = os.path.join(
+            os.getenv('ALLUSERSPROFILE', ''), 'nmap_scan')
+
+    # if log folder does not exist, create
+    os.makedirs(logDir, exist_ok=True)
+
+    return os.path.join(logDir, 'nmap_scan.log')
+
+
+def log_debug(text: object, verbose: bool = False) -> None:
+    logRecord: dict = dict()
+    now: datetime = datetime.now()
+    logRecord["timestamp"] = str(now.now())
+    logRecord["timezone"] = str(now.tzname())
+    logRecord["utc"] = str(now.utcnow())
+    logRecord["type"] = "nmap_scan"
+    logRecord["level"] = "debug"
+    logRecord["message"] = text
+    message: str = json.dumps(logRecord, sort_keys=True)
+    logging.debug(message)
+
+    if (verbose):
+        print(message)
+
+
+def log_info(text: object, verbose: bool = False) -> None:
+    logRecord: dict = dict()
+    now: datetime = datetime.now()
+    logRecord["timestamp"] = str(now.now())
+    logRecord["timezone"] = str(now.tzname())
+    logRecord["utc"] = str(now.utcnow())
+    logRecord["type"] = "nmap_scan"
+    logRecord["level"] = "info"
+    logRecord["message"] = text
+    message: str = json.dumps(logRecord, sort_keys=True)
+    logging.debug(message)
+
+    if (verbose):
+        print(message)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Cancelled by user.')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
