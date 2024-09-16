0688fd864deeb8b700caca63b8ec43331e4b32c3
Zafer Balkan <zafer@zaferbalkan.com>
2024-09-16 16:30:45 +0300

    Restructuring

--- nmap_scan.py
+++ nmap_scan.py
@@ -8,7 +8,6 @@
 # Do NOT include subnets with a network firewall in the path of the agent and the subnet.
 ################################
 import ctypes
-import ipaddress
 import json
 import logging
 import os
@@ -19,73 +18,14 @@ from datetime import datetime
 import nmap
 
 
-def is_admin() -> bool:
+def __is_admin() -> bool:
     if (platform.system() == "Windows"):
         return bool(ctypes.windll.shell32.IsUserAnAdmin()) != 0
     else:
         return (os.getuid() == 0)  # type: ignore
 
 
-def main() -> None:
-
-    # Running NMAP requires running as sudo/administrator
-    if (is_admin() == False):
-        raise Exception(
-            "This application requires root/administrator privileges.")
-
-    # Read and validate configuration
-    configPath: str = os.path.dirname(
-        os.path.realpath(__file__)) + "/config.json"
-    try:
-        with open(configPath, "r") as read_file:
-            tempConfig = json.load(read_file)
-    except:
-        raise Exception("Invalid config file")
-
-    subnets: list[str] = tempConfig.get("subnets")
-    verbose: bool = tempConfig.get("verbose")
-
-    source_label: str = tempConfig.get("source_label")
-    destination_label: str = tempConfig.get("destination_label")
-    arguments = tempConfig.get("args")
-
-    logpath: str = detect_logpath()
-
-    root_logger: logging.Logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
-    handler: logging.FileHandler = logging.FileHandler(logpath, 'a', 'utf-8')
-    handler.setFormatter(logging.Formatter('%(message)s'))
-    root_logger.addHandler(handler)
-
-    excepthook = log_error
-
-    # Initiate scan
-    scanner: nmap.PortScannerYield = nmap.PortScannerYield()
-    for subnet in subnets:
-
-        # Log per subnet
-        if (verbose):
-            log_debug(f"Starting scan against subnet: {subnet} with args: {arguments}",
-                      source_label=source_label,
-                      destination_label=destination_label,
-                      verbose=verbose)
-
-        for host, result in scanner.scan(subnet, arguments=arguments):
-            if (verbose):
-                print("Reporting host: " + host)
-            log_info(result,
-                     source_label=source_label,
-                     destination_label=destination_label,
-                     verbose=verbose)
-
-    if (verbose):
-        log_debug("Nmap scan completed.",
-                  source_label=source_label,
-                  destination_label=destination_label,
-                  verbose=verbose)
-
-
-def detect_logpath() -> str:
+def __detect_logpath() -> str:
     logDir: str = os.path.join('/var/log', 'nmap_scan')
     if (platform.system() == "Windows"):
         logDir = os.path.join(
@@ -135,20 +75,80 @@ def __log(text: object, level: str, source_label: str, destination_label: str, v
         print(json.dumps(json.loads(message), indent=4))
 
 
+def main() -> None:
+
+    # Running NMAP requires running as sudo/administrator
+    if (__is_admin() == False):
+        raise Exception(
+            "This application requires root/administrator privileges.")
+
+    # Read and validate configuration
+    configPath: str = os.path.dirname(
+        os.path.realpath(__file__)) + "/config.json"
+    try:
+        with open(configPath, "r") as read_file:
+            tempConfig = json.load(read_file)
+    except:
+        raise Exception("Invalid config file")
+
+    subnets: list[str] = tempConfig.get("subnets")
+    verbose: bool = tempConfig.get("verbose")
+
+    source_label: str = tempConfig.get("source_label")
+    destination_label: str = tempConfig.get("destination_label")
+    arguments = tempConfig.get("args")
+
+    # Initiate scan
+    scanner: nmap.PortScannerYield = nmap.PortScannerYield()
+    for subnet in subnets:
+
+        # Log per subnet
+        if (verbose):
+            log_debug(f"Starting scan against subnet: {subnet} with args: {arguments}",
+                      source_label=source_label,
+                      destination_label=destination_label,
+                      verbose=verbose)
+
+        for host, result in scanner.scan(subnet, arguments=arguments):
+            if (verbose):
+                print("Reporting host: " + host)
+            log_info(result,
+                     source_label=source_label,
+                     destination_label=destination_label,
+                     verbose=verbose)
+
+    if (verbose):
+        log_debug("Nmap scan completed.",
+                  source_label=source_label,
+                  destination_label=destination_label,
+                  verbose=verbose)
+
+
 # We assume the result is successful when user interrupted
 # the scan as it is an intentional act.
 # Otherwise, exit with an error code of 1.
 if __name__ == "__main__":
+    logpath: str = __detect_logpath()
+    root_logger: logging.Logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    handler: logging.FileHandler = logging.FileHandler(logpath, 'a', 'utf-8')
+    handler.setFormatter(logging.Formatter('%(message)s'))
+    root_logger.addHandler(handler)
+
+    excepthook = log_error
+
     try:
         main()
     except KeyboardInterrupt:
         print('Cancelled by user.')
+        logging.info('Cancelled by user.')
         try:
             sys.exit(0)
         except SystemExit:
             os._exit(0)
     except Exception as ex:
         print('ERROR: ' + str(ex))
+        logging.info('ERROR: ' + str(ex))
         try:
             sys.exit(1)
         except SystemExit:
